fb4d329aaa9a17014f3888af6676be634e594eea
PeterAJansen <pajansen@email.arizona.edu>
2023-11-22 22:28:00 -0700

    Working on python sandbox for evaluating hypotheses

---
+++ SandboxPython.py
@@ -0,0 +1,24 @@
+# SandboxPython.py
+
+
+
+
+# Class to run a sandboxed hypothesis represented as an 'IF statement' in Python, on the world state history (represented as an external JSON file)
+class SandboxPython():
+    # Constructor
+    def __init__(self, world):
+        self.world = world
+        
+
+    
+    def testHypothesis(hypothesisPythonStr:str):
+        # Step 1: Export the world state history to a JSON file
+
+        # Step 2: Export the hypothesis to a Python file
+
+        # Step 3: Run the hypothesis against the world state history, using a `FireJail` sandbox
+
+        # Step 4: Read the output of the sandbox, parsing the result (i.e. whether the hypothesis is true or not -- or rather, the proportion of the time that the hypothesis is true)
+
+
+        return 0.0    
