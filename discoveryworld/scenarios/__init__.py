401e384d3c717d6e35440dd48cfee200db5ffd7e
PeterAJansen <pajansen@email.arizona.edu>
2024-05-07 09:35:24 -0700

    minor refactoring

--- discoveryworld/scenarios/__init__.py
+++ discoveryworld/scenarios/__init__.py
@@ -7,7 +7,7 @@ from discoveryworld.scenarios.reactor_lab import makeScenarioReactorLab
 from discoveryworld.scenarios.town import makeScenarioTown
 from discoveryworld.scenarios.reactor_lab import makeScenarioReactorLab
 from discoveryworld.scenarios.tutorial import makeScenarioTutorial
-from discoveryworld.scenarios.smallskills import *
+from discoveryworld.scenarios.smallskills_dialog import *
 
 SCENARIOS = {
     "Town": makeScenarioTown,
