06c275d8eff9d7e37b8fcbf4892d43deb765b075
Marc-Alexandre Côté <marc.cote.19@gmail.com>
2024-10-09 20:19:16 -0400

    Fixing packaging.

--- setup.py
+++ setup.py
@@ -7,9 +7,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='discoveryworld',
+    description="DiscoveryWorld - Making new discovery.",
+    author='Peter Jansen, Marc-Alexandre Côté',
     version=open(os.path.join("discoveryworld", "version.py")).readlines()[0].split("=")[-1].strip("' \n"),
     packages=find_packages(),
     include_package_data=True,
-    description="DiscoveryWorld - Making new discovery.",
+    url="https://github.com/allenai/discoveryworld",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     install_requires=open('requirements.txt').readlines(),
+    python_requires='>=3.8',
 )
